17dd4dc9dde882865166a99d65f88cd82bd731e0
isapozhkov <i.sapozhkov.93@gmail.com>
2016-08-30 21:14:41 +0300

    Add methods add_network, parse_config, remove_network

--- reach_wifi_configurator.py
+++ reach_wifi_configurator.py
@@ -3,8 +3,10 @@ import shlex
 import time
 
 class ReachWiFi():
-	def __init__(self):
-		pass
+	def __init__(self, hostapd_path = "/etc/hostapd/hostapd.conf", wpasupplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"):
+		self.hostapd_path = hostapd_path
+		self.wpasupplicant_path = wpasupplicant_path
+		self.network_list = self.parse_wpa_conf_file()
 	def __del__(self):
 		pass
 	def write(self, args):
@@ -29,12 +31,11 @@ class ReachWiFi():
 	def wpa_sup_info(self):
 		self.write("wpa_supplicant -h")	
 
-	def connect(self, path = '/etc/wpa_supplicant/example.conf', interface = 'wlp6s0'):
-		self.write("wpa_supplicant -B -i " + interface + " -c " + path)
+	def start_wpa_supplicant(self, interface = 'wlan0'):
+		self.write("wpa_supplicant -B -i " + interface + " -c " + self.wpasupplicant_path)
 
 	def scan(self):
 		self.write("wpa_cli scan")
-		time.sleep(1)
 		res = self.write("wpa_cli scan_result").split("\n")[2:-1]
 
 		seq = []
@@ -43,16 +44,53 @@ class ReachWiFi():
 			seq.append((r1[0], r1[4]))
 		return seq	
 
+	def parse_wpa_conf_file(self):
+		res = list()
+		b = self.write("cat " + self.wpasupplicant_path).split("\n\n")[1:]
+		for c in b:
+			e = {n.split('=')[0] : n.split('=')[1] for n in c.split()[1:-1]}
+			#TODO: more effective
+			res.append((e.get("bssid") if e.get("bssid") is not None else '', 
+						e.get("ssid") if e.get("ssid") is not None else '', 
+						e.get("psk") if e.get("psk") is not None else ''))
+		return res
+
+	def delta_added_scan(self):
+		cur_scan = self.scan()
+		if self.network_list:
+			for key in self.network_list:
+				for a in cur_scan:
+					if key["ssid"] == a[1]:
+						cur_scan.pop(cur_scan.index(a))
+						break
+			return cur_scan
+		else:
+			return cur_scan
+
+	def add_network(self, args):
+		if self.network_list:
+			for key in self.network_list:
+				if args == key:
+					print "Already added"
+					return None
+		r = self.write("wpa_cli add_network").split("\n")[1]
+		self.write(['wpa_cli', 'set_network', r, 'bssid', args[0]])
+		self.write(['wpa_cli', 'set_network', r, 'ssid', '\"'+args[1]+'\"'])
+		self.write(['wpa_cli', 'set_network', r, 'psk', '\"'+args[2]+'\"'])
+		#self.write("wpa_cli enable_network %s" % r)
+		self.write("wpa_cli save_config")
+
+	def remove_network(self, ssid):
+		for a in self.network_list:
+			if a[2] == ssid:
+				ind = self.network_list.index(a)
+				break
+		self.network_list.pop(ind)
+		self.write("wpa_cli remove_network %s" % ind)
+		self.write("wpa_cli save_config")
+		self.write("wpa_cli reconfigure")
+
 
 if __name__ == '__main__':
-	a = ReachWiFi()
-	a.connect()
-	print a.scan()
-
-	r = a.write("wpa_cli add_network")
-	r = r.split("\n")[1]
-	print a.write(['wpa_cli', 'set_network', r, 'ssid', '\"EML33T2\"'])
-	print a.write(['wpa_cli', 'set_network', r, 'psk', '\"emrooftop\"'])
-	print a.write("wpa_cli enable_network %s" % r)
-	print a.write("wpa_cli save_config")
-	print a.write("wpa_cli quit")
+	a = ReachWiFi(wpasupplicant_path = "/etc/wpa_supplicant/example.conf")
+	#print a.scan()
