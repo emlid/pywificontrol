14c5683b6ea651c51a91f0a57b936d4ffc70fc15
isapozhkov <i.sapozhkov.93@gmail.com>
2016-09-07 15:28:31 +0300

    Delete some unexpected information

--- setup.py
+++ setup.py
@@ -10,30 +10,4 @@ setup(
     #license='LICENSE.txt',
     description='Module for contol WiFi connections with host(AP) and client(WPA) modes.',
     long_description=open('README.txt').read(),
-    classifiers=[
-        #   2 - Pre-Alpha
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 2 - Pre-Alpha',
-
-        'Intended Audience :: Developers',
-        'Topic :: System :: Networking',
-
-        #'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
-    ],
-    install_requires=[
-        "wpa_supplicant",
-        "hostapd",
-    ],
 )
