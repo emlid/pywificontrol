3e4d449d6c6c40aa02b506a541a70fc753226c7a
isapozhkov <i.sapozhkov.93@gmail.com>
2016-09-07 16:06:39 +0300

    Change version

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 
 setup(
     name='wificontrol',
-    version='0.0.1.dev1',
+    version='0.0.1.dev',
     author='Ivan Sapozhkov',
     author_email='i.sapozhkov.93@gmail.com',
     py_modules=['wificontrol'],
-    #packages=['wificontrol'],
     url='https://github.com/emlid/reach-wifi-configurator.git',
-    #license='LICENSE.txt',
     description='Module for contol WiFi connections with host(AP) and client(WPA) modes.',
     long_description=open('README.md').read(),
 )
