e6a7fd7c93193e8c3e03481e9ad3c4eff1d34f71
isapozhkov <i.sapozhkov.93@gmail.com>
2016-09-29 17:16:49 +0300

    New release v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='wificontrol',
-    version='0.1.0',
+    version='0.1.1',
     author='Ivan Sapozhkov',
     author_email='ivan.sapozhkov@emlid.com',
     py_modules=['wificontrol'],
