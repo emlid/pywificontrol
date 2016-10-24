c70d3249221bed3e59632179de61f289f8489502
isapozhkov <i.sapozhkov.93@gmail.com>
2016-10-24 16:20:28 +0300

    New release. Fux bugs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='wificontrol',
-    version='0.2.0',
+    version='0.2.1',
     author='Ivan Sapozhkov',
     author_email='ivan.sapozhkov@emlid.com',
-    py_modules=['wificontrol'],
+    packages=['wificontrol'],
     license = 'GPLv3',
     url='https://github.com/emlid/reach-wifi-configurator.git',
     description='Module for contol WiFi connections with host(AP) and client(WPA) modes.',
