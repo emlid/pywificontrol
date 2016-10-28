4c0ddb04adbbafe817a6ffa398dba2a7a2aecab8
isapozhkov <i.sapozhkov.93@gmail.com>
2016-10-28 12:53:33 +0300

    New release v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wificontrol',
-    version='0.2.1',
+    version='0.2.22',
     author='Ivan Sapozhkov',
     author_email='ivan.sapozhkov@emlid.com',
     packages=['wificontrol'],
