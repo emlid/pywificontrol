fb2d9d83473c2d8fb536d1605aa36c7e41922d1b
isapozhkov <i.sapozhkov.93@gmail.com>
2016-11-21 12:59:54 +0300

    Add utils package

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='wificontrol',
-    version='0.2.1',
+    version='0.2.8',
     author='Ivan Sapozhkov',
     author_email='ivan.sapozhkov@emlid.com',
-    packages=['wificontrol'],
+    packages=['wificontrol', 'wificontrol.utils'],
     license = 'GPLv3',
     url='https://github.com/emlid/reach-wifi-configurator.git',
     description='Module for contol WiFi connections with host(AP) and client(WPA) modes.',
