5b4d76798bd73018a0e70ebde5f6f6f2f50e8e5c
isapozhkov <i.sapozhkov.93@gmail.com>
2016-11-21 12:37:50 +0300

    Add WpaTemplates class

--- wificontrol/utils/__init__.py
+++ wificontrol/utils/__init__.py
@@ -1,5 +1,5 @@
 from .fileupdater import ConfigurationFileUpdater, NullFileUpdater
-from .wpasupplicant import WpaSupplicantInterface, WpaSupplicantNetwork
+from .wpasupplicant import WpaSupplicantInterface, WpaSupplicantNetwork, WpaTemplates
 
 from .fileupdater import FileError
 from .wpasupplicant import ServiceError, InterfaceError, PropertyError
