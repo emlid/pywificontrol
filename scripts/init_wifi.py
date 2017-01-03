8f801f92bdb123d063d5a6029d518a5567419124
isapozhkov <i.sapozhkov.93@gmail.com>
2017-01-03 22:23:42 +0300

    Correct print function in initial script

--- scripts/init_wifi.py
+++ scripts/init_wifi.py
@@ -26,41 +26,41 @@ from wificontrol import WiFiControl, WiFiControlError
 
 def ConnectionCallback(result, wific):
     if not result:
-        print "Can't connect to any network."
-        print "Start HOSTAP mode"
+        print("Can't connect to any network.")
+        print("Start HOSTAP mode")
         try:
             wific.start_host_mode()
         except WiFiControlError as error:
-            print error
+            print(error)
             sys.exit(2)
         else: 
-            print "In HOST mode"
+            print("In HOST mode")
             sys.exit(10)
     else:
         status = wific.get_status()[1]
-        print "Connected to {}".format(status['ssid'])
+        print("Connected to {}".format(status['ssid']))
         sys.exit(0)
 
 if __name__ == "__main__":
     try:
         rwc = wificontrol()
     except OSError, error:
-        print error
+        print(error)
         sys.exit(2)
     else:
-        print "Start wpa_supplicant service"
+        print("Start wpa_supplicant service")
         try:
             rwc.start_client_mode()
         except WiFiControlError as error:
-            print error
+            print(error)
             try:
                 rwc.start_host_mode()
             except WiFiControlError as error:
-                print error
+                print(error)
                 sys.exit(2)
             else:
-                print "In HOST mode"
+                print("In HOST mode")
                 sys.exit(10)
         else:
-            print "Start connecting to networks"
+            print("Start connecting to networks")
             rwc.start_connecting(None, callback=ConnectionCallback, args = rwc)
