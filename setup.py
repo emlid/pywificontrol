09dd05738ca4651a7cdeb34b459b05efef4de5dd
Denis Chagin <chaginden@gmail.com>
2017-09-14 14:17:04 +0300

    repo: Add missing license comments

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 setup(
     name='wificontrol',
     version='0.4.0',
-    author='Ivan Sapozhkov, Denis Changin',
+    author='Ivan Sapozhkov, Denis Chagin',
     author_email='ivan.sapozhkov@emlid.com, denis.chagin@emlid.com',
     packages=['wificontrol', 'wificontrol.utils'],
     license='GPLv3',
     url='https://github.com/emlid/reach-wifi-configurator.git',
     description='Module for control WiFi connections with host(AP) and client(WPA) modes.',
     install_requires=[
-        'reachstatus',
         'sysdmanager',
         'netifaces'
     ],
