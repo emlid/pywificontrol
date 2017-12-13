8c763adc6f83e6c05c343fbea31f6485714691fb
Egor Fedorov <egor.fedorov@emlid.com>
2017-12-13 12:56:39 +0300

    setup.py: update version to v0.5.0, add the newest author

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='wificontrol',
-    version='0.4.0',
-    author='Ivan Sapozhkov, Denis Chagin',
+    version='0.5.0',
+    author='Ivan Sapozhkov, Denis Chagin, Dmitriy Skorykh',
     author_email='denis.chagin@emlid.com',
     packages=find_packages(exclude=['tests']),
     license='BSD-3',
